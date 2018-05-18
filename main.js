8704ed9335341b842056e9fd7089642f75768e3d
McNeeM2018 <mmcnee@sjnma.org>
2018-05-18 09:07:03 -0500

    I made a madlib

---
+++ main.js
@@ -0,0 +1,21 @@
+alert("hello!");
+alert("This is a mad lib answer the questions");
+alert("Bye");
+
+
+
+var noun1 = prompt("Please enter a noun","");
+var noun2 = prompt("Please enter a noun","");
+var noun3 = prompt("Please enter a noun","");
+var noun4 = prompt("Please enter a noun","");
+
+var verb1 = prompt("Please enter a verb","");
+var verb2 = prompt("Please enter a ver ending in ing","");
+var verb3 = prompt("Please enter a verb","");
+var verb4 = prompt("Please enter a verb ending in ing","");
+
+var adjective1 = prompt("Please enter a adjective","");
+var adjective2 = prompt("Please enter a adjective","");
+
+document.getElementById("name").innerHTML = "The "+noun1+verb1+" the cat. It was a very "+adjective1+" day. The cat was "+adjective2+" all over the floor. He ate a "+noun2+ "and a rat. He did this because he was" +verb2+. "On the other hand the dog made a" +noun3+ "while he was" +verb3+." He got stuck in a" +noun4+ "and died. This story was made by me" +verb4+.
+
